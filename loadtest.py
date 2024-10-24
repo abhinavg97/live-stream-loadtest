da4dfef760160a06ad606eb2cf70128cc8a1f1bf
abhinavg97 <gabhinav001@gmail.com>
2024-10-24 18:14:02 -0400

    load test server

---
+++ loadtest.py
@@ -0,0 +1,17 @@
+from locust import HttpUser, task, between
+
+
+class VideoStreamUser(HttpUser):
+    wait_time = between(1, 5)
+
+    @task
+    def load_video(self):
+        # Request the HLS .m3u8 file
+        response = self.client.get("/stream")
+        # Simulate browser requesting HLS segments by parsing m3u8 file
+        m3u8_content = response.text
+        segments = [line for line in m3u8_content.splitlines() if line.endswith('.ts')]
+        for segment in segments:
+            self.client.get(f"/{segment}")
+
+# Usage: locust -f this_file.py --host=http://localhost:8000
